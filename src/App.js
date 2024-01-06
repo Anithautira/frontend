af058bee7d4d9912a7082ff61238cf8e5e91c72d
Anithautira <142482568+Anithautira@users.noreply.github.com>
2024-01-06 18:31:04 +0800

    capstone-gmail-frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage.js';
+import MailPage from './pages/MailPage.js';
+import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/inbox' element={
+          <ProtectedRoute>
+            <MailPage />
+          </ProtectedRoute>
+        } />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
+}
+
+function ProtectedRoute({ children }) {
+  const isAuth = localStorage.getItem("token");
+  return isAuth ? children : <Navigate replace to="/" />
 }
 
 export default App;
---
+++ src/components/temp/EmailData.js
@@ -0,0 +1,24 @@
+export const emailData = [
+    {
+        starred: true,
+        from: "GUVI-FSD",
+        subject: "Capstone Project Submission",
+        message: "Hi, Anithasri, your capstone project is Gmail Clone",
+        time: "1 Feb",
+        read: true
+    }, {
+        starred: false,
+        from: "Zen-Class",
+        subject: "B40 AWS Session @ 8 Am Confirmation",
+        message: "Anithasri Lakshmanan, Thank for registering B40 AWS Session @ 8 Am",
+        time: "5 Jan",
+        read: false
+    }, {
+        starred: false,
+        from: "ICICI-Bank",
+        subject: "Estatment",
+        message: "ICICI Bank Statement from 01-01-2023 to 31-01-2023",
+        time: "8 Jan",
+        read: false
+    }
+]
